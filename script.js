e11ddedebf00510c7abb7e1603c0b52198e984ce
utkarshkushwah001-hash <utkarshkushwah001@gmail.com>
2026-06-20 15:38:50 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,33 @@
-body{
- text-align:center;
- font-family:Arial;
- padding:20px;
-}
+let steps = 0;
+let lastX = 0;
+let threshold = 12;
 
-.card{
- border:1px solid black;
- padding:20px;
- margin-top:20px;
-}
+document.getElementById("startBtn").addEventListener("click", () => {
+
+  alert("Tracking started");
+
+  if (typeof DeviceMotionEvent !== "undefined") {
+
+    window.addEventListener("devicemotion", (event) => {
+      let x = event.accelerationIncludingGravity.x;
+
+      if (Math.abs(x - lastX) > threshold) {
+        steps++;
+
+        let calories = steps * 0.04;
+        let distance = steps * 0.75;
+
+        document.getElementById("steps").innerText = steps;
+        document.getElementById("calories").innerText =
+          calories.toFixed(2);
+        document.getElementById("distance").innerText =
+          distance.toFixed(2);
+      }
+
+      lastX = x;
+    });
+
+  } else {
+    alert("Motion sensor not supported");
+  }
+});
